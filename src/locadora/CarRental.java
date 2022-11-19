c71090cfd60de7c68707ce74ec65d39622163c1a
Filipe <filipeandrade000@gmail.com>
2022-11-18 23:20:47 -0300

    Adicionando Classes Lotacao

--- src/locadora/CarRental.java
+++ src/locadora/CarRental.java
@@ -1,5 +1,36 @@
 package locadora;
 
+import javax.xml.crypto.Data;
+
 public class CarRental {
 
+	private Data start;
+	private Data finish;
+	
+	public CarRental() {
+		
+	}
+
+	public CarRental(Data start, Data finish) {
+		this.start = start;
+		this.finish = finish;
+	}
+
+	public Data getStart() {
+		return start;
+	}
+
+	public void setStart(Data start) {
+		this.start = start;
+	}
+
+	public Data getFinish() {
+		return finish;
+	}
+
+	public void setFinish(Data finish) {
+		this.finish = finish;
+	}
+
+	
 }
