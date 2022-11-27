ba20d1f5f953e841082adea97572268011866a0d
Filipe <filipeandrade000@gmail.com>
2022-11-27 17:25:29 -0300

    adicionando atualizacoes

--- src/exemplos/Exemplo.java
+++ src/exemplos/Exemplo.java
@@ -1,12 +1,31 @@
 package exemplos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exemplo {
-	
-	 public static void main(String[] args){
-	        int[] arr=new int[10];
-	        for(int i=0;i<arr.length;i++){
-	            arr[i]=(i+1)*(i+1);
-	        }
-	        System.out.println(arr[9]+arr[10]);
-	    }
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		List<String> list = new ArrayList<>();
+		String corAtual = null;
+		
+		do {
+			System.out.print("Digite uma cor: ");
+			corAtual = sc.nextLine();
+			list.add(corAtual);
+			
+			
+		}while(!corAtual.equals("preto"));
+		
+		for(String s : list) {
+			System.out.println("Cor: "+s);
+		}
+		
+		sc.close();
+		
+	}
 }
