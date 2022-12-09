c5d7a2d2ff9ac0eb505252095c69f6c5fbb75d0c
Filipe <filipeandrade000@gmail.com>
2022-12-09 05:56:38 -0300

    declarada e inline

--- src/lambdas/lambdaProgram1.java
+++ src/lambdas/lambdaProgram1.java
@@ -3,6 +3,7 @@ package lambdas;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class lambdaProgram1 {
@@ -16,8 +17,9 @@ public class lambdaProgram1 {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		//Function<Product, String> func = p-> p.getName().toUpperCase();
 		//map só funciona pra stream
-		List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+		List<String> names = list.stream().map(p-> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
