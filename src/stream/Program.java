fceabec1f92ef1d9ad9b068d611055224012877b
Filipe <filipeandrade000@gmail.com>
2022-12-09 22:28:31 -0300

    pipeline

--- src/stream/Program.java
+++ src/stream/Program.java
@@ -2,6 +2,7 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -10,26 +11,22 @@ public class Program {
 		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		
-		Stream<Integer> st1 = list.stream();
-		// tString converte stream em vetor
-		System.out.println(Arrays.toString(st1.toArray()));
-		//map aplica uma função no stream
-		Stream<Integer> st2 = list.stream().map(x -> x*10);
-		System.out.println(Arrays.toString(st2.toArray()));
+		//Pipeline
+		Stream<Integer> str1 = list.stream().map(x->x*10); 
+		System.out.println(Arrays.toString(str1.toArray()));
 		
-		//of permite colocar os valores diretamente
-		Stream<String> st3 = Stream.of("Maria","Jose","Alex","Filipe");
-		System.out.println(Arrays.toString(st3.toArray()));
+		//reduce é tipo o +=, para multiplição deveria comecar com 1
+		int sum = list.stream().reduce(0, (x,y)->x+y);
+		System.out.println("Sum = "+ sum);
 		
-		//iterate função de iteração (primeiro item, função)
-		Stream<Integer> st4 = Stream.iterate(0, x -> x+2);
-		//como é infinita, é necessária usar limit
-		System.out.println(Arrays.toString(st4.limit(10).toArray()));
-		
-		//Sequencia de fibonacci
-		//precisa do map, pois o long retorna pares
-		Stream<Long> fibonacci = Stream.iterate(new Long[] {0L,1L}, p-> new Long[] {p[1],p[0]+p[1]}).map(p->p[0]);
-		System.out.println(Arrays.toString(fibonacci.limit(10).toArray()));
+		//Filtrar quem satifaz o predicado
+		//map multiplica estes filtros por 10
+		//collect transforma stream em list
+		List<Integer> newList = list.stream()
+				.filter(x-> x%2==0)
+				.map(x->x*10)
+				.collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
 	
 	}
 
